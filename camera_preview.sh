d952f17d83aa6465172057eb939c49145375dc77
JoCoLibraryMakerSpace <jocolibrarymakerspace@gmail.com>
2018-12-04 15:01:07 -0600

    Code cleanup in preparation for Instructables publishing.

--- camera_preview.sh
+++ camera_preview.sh
@@ -1,28 +1,22 @@
+#This project is based on Phillip Burgess' "Instant Camera using Raspberry Pi and Thermal Printer".
+#You can find his complete tutorial on Adafruit at https://learn.adafruit.com/instant-camera-using-raspberry-pi-and-thermal-printer?view=all
+#You can find our tutorial for this project, including a laser cut frame, at [insert URL].
+
 #!/bin/bash
 
+#We tell the Raspberry Pi on which pin the shutter button is connected
 SHUTTER=16
-HALT=21
-LED=5
 
-# Initialize GPIO states
+# We get the GPIO pin states ready
 gpio -g mode  $SHUTTER up
-gpio -g mode  $HALT    up
-gpio -g mode  $LED     out
-
-# Flash LED on startup to indicate ready state
-for i in `seq 1 5`;
-do
-	gpio -g write $LED 1
-	sleep 0.2
-	gpio -g write $LED 0
-	sleep 0.2
-done   
 
 while :
 do
-	# Check for shutter button
+	# The Raspberry Pi checks if the shutter button has been pressed
 	if [ $(gpio -g read $SHUTTER) -eq 0 ]; then
-		gpio -g write $LED 1
+		#gpio -g write $LED 1
+		#The Raspberry Pi displays a preview picture and after 2 seconds, sends it to the printer.
+		#Change the timeout value in milliseconds to change the preview time. 2000 = 2 seconds, 3000 = 3 seconds, etc.
 		raspistill -br 75 --fullscreen --timeout 2000 -o - | lp
 		sleep 1
 		# Wait for user to release button before resuming
@@ -30,15 +24,15 @@ do
 		gpio -g write $LED 0
 	fi
 
-	# Check for halt button
-	if [ $(gpio -g read $HALT) -eq 0 ]; then
-		# Must be held for 2+ seconds before shutdown is run...
-		starttime=$(date +%s)
-		while [ $(gpio -g read $HALT) -eq 0 ]; do
-			if [ $(($(date +%s)-starttime)) -ge 2 ]; then
-				gpio -g write $LED 1
-				shutdown -h now
-			fi
-		done
-	fi
+	# # The Raspberry Pi checks if the shutdown button has been pressed
+	# if [ $(gpio -g read $HALT) -eq 0 ]; then
+		# # Must be held for 2+ seconds before shutdown is run...
+		# starttime=$(date +%s)
+		# while [ $(gpio -g read $HALT) -eq 0 ]; do
+			# if [ $(($(date +%s)-starttime)) -ge 2 ]; then
+				# gpio -g write $LED 1
+				# shutdown -h now
+			# fi
+		# done
+	# fi
 done
